3b989d7e521897b62556abf6e567ccf70ddd3f22
lalit <lalitbisht540@gmail.com>
2023-07-19 15:59:00 +0530

    added recursive insert functionality in Singly Linked List

--- LinkedList/SinglyLinkedList.cpp
+++ LinkedList/SinglyLinkedList.cpp
@@ -2,136 +2,182 @@
 #include <iostream>
 using namespace std;
 
-class Node {
-    public:
-        int val;
-        Node* next;
-    
-    public:
-        Node() {
-            this->val = 0;
-            next = NULL;
-        }
-        Node(int val) {
-            this->val = val;
-            this->next = NULL;
-        }
-        Node(int val, Node* next) {
-            this->val = val;
-            this->next = next;
-        }
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+public:
+    Node()
+    {
+        this->val = 0;
+        next = NULL;
+    }
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+    Node(int val, Node *next)
+    {
+        this->val = val;
+        this->next = next;
+    }
 };
 
-class LinkedList {
-    public:
-        int size;
-        Node* head;
-        Node* tail;
-    public:
-        LinkedList() {
-            this->size = 0; 
-            head = NULL;
-            tail = NULL;
+class LinkedList
+{
+public:
+    int size;
+    Node *head;
+    Node *tail;
+
+public:
+    LinkedList()
+    {
+        this->size = 0;
+        head = NULL;
+        tail = NULL;
+    }
+
+    void insertFirst(int val)
+    {
+        Node *node = new Node(val);
+        node->next = head;
+        head = node;
+        if (tail == NULL)
+        {
+            tail = node;
         }
-        
-        void insertFirst(int val) {
-            Node* node = new Node(val);
-            node->next = head;
-            head = node;
-            if(tail == NULL) {
-                tail = node;
-            }
-            size += 1;
+        size += 1;
+    }
+
+    void insertEnd(int val)
+    {
+        Node *node = new Node(val);
+        // Node* temp = head;
+        // while(temp->next!=NULL) {
+        //     temp = temp->next;
+        // }
+        // temp->next = node;
+        if (tail == NULL)
+        {
+            insertFirst(val);
+            return;
         }
-        
-        void insertEnd(int val) {
-            Node* node = new Node(val);
-            // Node* temp = head;
-            // while(temp->next!=NULL) {
-            //     temp = temp->next;
-            // }
-            // temp->next = node;
-            if(tail==NULL) {
-                insertFirst(val);
-                return;
-            }
-            tail->next = node;
-            tail = tail->next;
-            size+=1;
+        tail->next = node;
+        tail = tail->next;
+        size += 1;
+    }
+
+    void insertAtInd(int val, int ind)
+    {
+        if (ind == 0)
+        {
+            insertFirst(val);
+            return;
         }
-        
-        void insertAtInd(int val, int ind) {
-            if(ind==0) {
-                insertFirst(val);
-                return;
-            }else if(ind == size) {
-                insertEnd(val);
-                return;
-            }
-            Node* node = new Node(val);
-            Node* temp = head;
-            while(ind>1) {
-                temp = temp->next;
-                ind--;
-            }
-            node->next = temp->next;
-            temp->next = node;
-            size+=1;
+        else if (ind == size)
+        {
+            insertEnd(val);
+            return;
         }
-        
-        int deleteFirst() { 
-            int val = head->val;
-            head= head->next;
-            if(head==NULL) {
-                tail = head;
-            }
-            size-=1;
-            return val;
+        Node *node = new Node(val);
+        Node *temp = head;
+        while (ind > 1)
+        {
+            temp = temp->next;
+            ind--;
         }
-        
-        int deleteEnd() {
-            int val = tail->val;
-            if(size<=1) {
-                return deleteFirst();
-            }
-            Node* temp = head;
-            while(temp->next != tail) {
-                temp = temp->next;
-            }
-            tail = temp;
-            tail->next = NULL;
-            size-=1;
-            return val;
+        node->next = temp->next;
+        temp->next = node;
+        size += 1;
+    }
+
+    void insertRec(int val, int ind)
+    {
+        head = insertRec(val, ind, head);
+    }
+
+    Node *insertRec(int val, int ind, Node *node)
+    {
+        if (ind == 0)
+        {
+            Node *temp = new Node(val, node);
+            size++;
+            return temp;
         }
-        
-        int deleteAtInd(int ind) {
-            if(ind == 0) {
-                return deleteFirst();
-            }else if(ind == size) {
-                return deleteEnd();
-            }
-            Node* temp = head;
-            while(ind>1) {
-                temp = temp->next;
-                ind--;
-            }
-            int val = temp->next->val;
-            temp->next = temp->next->next;
-            size-=1;
-            return val;
+        node->next = insertRec(val, ind - 1, node->next);
+        return node;
+    }
+
+    int deleteFirst()
+    {
+        int val = head->val;
+        head = head->next;
+        if (head == NULL)
+        {
+            tail = head;
         }
-        
-        void displayLL() {
-            Node* temp = head;
-            while(temp!=NULL) {
-                cout << temp->val << " --> ";
-                temp = temp->next;
-            }
-            cout << "END" <<endl;
+        size -= 1;
+        return val;
+    }
+
+    int deleteEnd()
+    {
+        int val = tail->val;
+        if (size <= 1)
+        {
+            return deleteFirst();
         }
+        Node *temp = head;
+        while (temp->next != tail)
+        {
+            temp = temp->next;
+        }
+        tail = temp;
+        tail->next = NULL;
+        size -= 1;
+        return val;
+    }
+
+    int deleteAtInd(int ind)
+    {
+        if (ind == 0)
+        {
+            return deleteFirst();
+        }
+        else if (ind == size)
+        {
+            return deleteEnd();
+        }
+        Node *temp = head;
+        while (ind > 1)
+        {
+            temp = temp->next;
+            ind--;
+        }
+        int val = temp->next->val;
+        temp->next = temp->next->next;
+        size -= 1;
+        return val;
+    }
+
+    void displayLL()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->val << " --> ";
+            temp = temp->next;
+        }
+        cout << "END" << endl;
+    }
 };
 
-int main() {
+int main()
+{
     // Write C++ code here
     LinkedList lst;
     lst.insertFirst(1);
@@ -139,7 +185,7 @@ int main() {
     lst.insertFirst(3);
     lst.insertFirst(4);
     lst.insertEnd(10);
-    lst.insertAtInd(100,3);
+    lst.insertAtInd(100, 3);
     // cout << lst.size << endl;
     lst.displayLL();
     cout << lst.deleteFirst() << " deleted!" << endl;
@@ -147,6 +193,8 @@ int main() {
     cout << lst.deleteEnd() << endl;
     lst.displayLL();
     cout << lst.deleteAtInd(2) << endl;
+    lst.displayLL();
+    lst.insertRec(58, 2);
     lst.displayLL();
     return 0;
 }
